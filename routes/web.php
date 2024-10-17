463900bec932e2f4a8b7c999938bd0564a6f6169
InformaticoJS <informaticowebjs@gmail.com>
2024-10-17 10:56:03 -0500

    cambio 3

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// alalau de mrd
+// ventanas
 Route::get('login',function(){
     return view("Este es el login");
 });

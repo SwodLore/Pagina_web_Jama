8aa6c960669b7f03eb8df0a2131723aab9be39ef
SwodLore <apovesmartinez@gmail.com>
2024-10-17 10:54:28 -0500

    comentario

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// las ventanas
+// alalau de mrd
 Route::get('login',function(){
     return view("Este es el login");
 });

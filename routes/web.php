f3710db4b9e42fc58e0aa60b848fd91d58094d33
InformaticoJS <informaticowebjs@gmail.com>
2024-10-17 10:52:46 -0500

    comentario

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+// las ventanas
 Route::get('login',function(){
     return view("Este es el login");
 });
